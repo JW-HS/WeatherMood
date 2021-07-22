2e3b261c531bd7399f1f4b59dc129256ce1e227e
JiWon Lee <jiwon.swdev@gmail.com>
2021-07-23 00:03:07 +0900

    [#10] feat: ViewControllers 생성

---
+++ WeatherMood/ViewController/EmoticonSelectingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmoticonSelectingViewController.swift
+//  WeatherMood
+//
+//  Created by 이지원 on 2021/07/23.
+//
+
+import UIKit
+
+final class EmoticonSelectingViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
