96112ca9f8160d969604adee3eaf231ac76b3376
JiWon Lee <jiwon.swdev@gmail.com>
2021-08-05 09:38:06 +0900

    [#35] view: Styles.grid 주석 추가
    Co-authored-by: HyunSu Park <48749182+gustn3965@users.noreply.github.com>

--- WeatherMood/Utilities/Styles.swift
+++ WeatherMood/Utilities/Styles.swift
@@ -9,7 +9,7 @@ import UIKit
 
 enum Styles {
     static let cornerRadius: CGFloat = 15.0
-
+    /// 4.0를 곱한만큼 CGFloat값을 return한다.
     static func grid(_ count: Int) -> CGFloat {
         4.0 * CGFloat(count)
     }
